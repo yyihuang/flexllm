c791cc2b42e910db4f31d3a8ad1249336ec7808c
Gabriele Oliaro <goliaro@cs.cmu.edu>
2025-04-10 15:25:16 -0700

    update

--- benchmarking/get_sharegpt_trace.py
+++ benchmarking/get_sharegpt_trace.py
@@ -286,11 +286,11 @@ def save_trace(trace: Trace, output_path: str):
     print(f"Trace saved to {output_path}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Build WildChat trace")
+    parser = argparse.ArgumentParser(description="Build ShareGPT trace")
     parser.add_argument("--model_name", type=str, default="meta-llama/Llama-3.1-70B-Instruct", help="Model name")
     parser.add_argument("-m", "--max-length", type=int, default=8192, help="Maximum prompt + response length")
-    parser.add_argument("-n", "--num_entries", type=int, default=1000, help="Number of entries")
-    parser.add_argument("-s", "--seed", type=int, default=0, help="Random seed")
+    parser.add_argument("-n", "--num_entries", type=int, default=1500, help="Number of entries")
+    parser.add_argument("-s", "--seed", type=int, default=42, help="Random seed")
     parser.add_argument("-o", "--output_file", type=str, default="./traces/sharegpt.json", help="Output file name")
     parser.add_argument("-t", "--trace-type", type=str, choices=["offline", "poisson", "splitwise"], default="offline", help="Arrival Times Trace Type")
     parser.add_argument("-a", "--arrival-rate", type=float, default=0.0, help="Arrival Rate")
---
+++ benchmarking/offline/benchmark_flexllm.sh
@@ -0,0 +1,74 @@
+set -x
+set -e
+
+# Cd into directory holding this script
+cd "${BASH_SOURCE[0]%/*}/../../flexflow-serve/build"
+source ./set_python_envs.sh
+
+MODEL_NAMES=(
+#   "meta-llama/Llama-3.1-8B-Instruct"
+  "mistralai/Mistral-Small-24B-Instruct-2501"
+  "meta-llama/Llama-3.1-70B-Instruct"
+)
+TP_DEGREES=(
+#   1
+  2
+  4
+)
+TRACES=(
+  sharegpt
+  # wildchat
+)
+NCPUS=16
+FSIZE=76000
+ZSIZE=200000
+CSIZE=2048
+MAX_SEQ_LEN=8192
+NUM_KV_CACHE_SLOTS=300000
+BATCH_SIZE=256
+MAX_TOKENS_PER_BATCH=256
+
+OUTPUT_FOLDER="../../benchmarking/output/offline/flexllm"
+TRACES_FOLDER="../../benchmarking/traces"
+
+
+mkdir -p $OUTPUT_FOLDER/output
+mkdir -p $OUTPUT_FOLDER/logs
+mkdir -p $OUTPUT_FOLDER/profiling
+
+for trace in "${TRACES[@]}"; do
+    
+    for i in "${!MODEL_NAMES[@]}"; do
+        MODEL_NAME=${MODEL_NAMES[$i]}
+        NGPUS=${TP_DEGREES[$i]}
+
+        if [ "$MODEL_NAME" == "mistralai/Mistral-Small-24B-Instruct-2501" ]; then
+            TRACE_FILE="${TRACES_FOLDER}/${trace}_mistral.json"
+        else
+            TRACE_FILE="${TRACES_FOLDER}/${trace}.json"
+        fi
+        test -f $TRACE_FILE || { echo "File $TRACE_FILE not found"; exit 1; }
+        
+        OUTPUT_FILE="${OUTPUT_FOLDER}/output/${MODEL_NAME//\//_}_${trace}_bz_${BATCH_SIZE}_tokens_per_batch_${MAX_TOKENS_PER_BATCH}_kv_cache_slots_${NUM_KV_CACHE_SLOTS}.json"
+        rm $OUTPUT_FILE || true
+        
+        echo "Running $MODEL_NAME (tp=$NGPUS) on $trace with BZ=$BATCH_SIZE, TOKENS_PER_BATCH=$MAX_TOKENS_PER_BATCH, KV_CACHE_SLOTS=$NUM_KV_CACHE_SLOTS"
+        
+        time ./inference/incr_decoding/incr_decoding \
+            -ll:cpu $NCPUS -ll:gpu $NGPUS -ll:util $NCPUS \
+            -ll:fsize $FSIZE -ll:zsize $ZSIZE -ll:csize $CSIZE \
+            -llm-model $MODEL_NAME --fusion \
+            -tensor-parallelism-degree $NGPUS \
+            -prompt $TRACE_FILE \
+            -output-file $OUTPUT_FILE \
+            -profiling-folder "${OUTPUT_FOLDER}/profiling" \
+            --max-requests-per-batch $BATCH_SIZE \
+            --max-tokens-per-batch $MAX_TOKENS_PER_BATCH \
+            --max-sequence-length $MAX_SEQ_LEN \
+            --num-kv-cache-slots $NUM_KV_CACHE_SLOTS \
+            --ignore-eos --warmup
+        
+        sleep 5
+
+    done
+done
