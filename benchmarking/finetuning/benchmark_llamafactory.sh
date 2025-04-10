642e37218f8543054045a2fc02bae38230603f29
Gabriele Oliaro <goliaro@cs.cmu.edu>
2025-04-09 18:51:23 -0700

    update

--- benchmarking/finetuning/benchmark_llamafactory.sh
+++ benchmarking/finetuning/benchmark_llamafactory.sh
@@ -9,6 +9,6 @@ cd "${BASH_SOURCE[0]%/*}/../../LLaMA-Factory"
 # Single GPU LLAMA-3.1 8B
 # CUDA_VISIBLE_DEVICES=0 llamafactory-cli train examples/flexllm/t1_8B.yaml
 # Multi GPU (2) QWEN-2.5 32B
-CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train examples/flexllm/t1_32B.yaml
+CUDA_VISIBLE_DEVICES=0,1 llamafactory-cli train examples/flexllm/t1_mistral.yaml
 # Multi GPU (4) LLAMA-3.1 70B
-CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train examples/flexllm/t1_70B.yaml
+# CUDA_VISIBLE_DEVICES=0,1,2,3 llamafactory-cli train examples/flexllm/t1_70B.yaml
